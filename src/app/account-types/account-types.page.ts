59773984b8721c6e0ae5e1a29e301fc634f2b113
isaacrabin <isaacmrongo@gmail.com>
2024-06-18 14:46:16 +0300

    ui updtaes

--- src/app/account-types/account-types.page.ts
+++ src/app/account-types/account-types.page.ts
@@ -19,4 +19,19 @@ export class AccountTypesPage implements OnInit {
     this.router.navigate(['account-products']);
   }
 
+  getGreeting(): string {
+    const now = new Date();
+    const hours = now.getHours();
+
+    if (hours >= 5 && hours < 12) {
+        return "Good morning";
+    } else if (hours >= 12 && hours < 17) {
+        return "Good afternoon";
+    } else if (hours >= 17 && hours < 21) {
+        return "Good evening";
+    } else {
+        return "Hello";
+    }
+}
+
 }
